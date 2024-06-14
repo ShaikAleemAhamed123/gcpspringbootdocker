88e8b16cd1fe3b4b0446daf59770c48ed53c76a5
ShaikAleemAhamed123 <shaikaleemahamed786@gmail.com>
2024-06-14 18:43:21 +0530

    initial commit

---
+++ src/main/java/com/gcp/demo/controller.java
@@ -0,0 +1,17 @@
+package com.gcp.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.service.annotation.GetExchange;
+
+@Controller
+public class controller {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String homeRoute(){
+        return "Hello World !";
+    }
+}
